9467f881255d9efa071a88e5069e6b619fde5e6f
hector Rivera <hrolriverav@gmail.com>
2024-03-15 13:40:09 -0600

    thread y archivo de txt

---
+++ src/lab/pkg9p2_hectorrivera/adminTextFile.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab.pkg9p2_hectorrivera;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author hriverav
+ */
+public class adminTextFile {
+    private File filetxt = null;
+    private String txt = "";
+
+    public adminTextFile() {
+    }
+    public adminTextFile(String filepath) {
+        this.filetxt = new File(filepath);
+    }
+
+    public File getRom() {
+        return filetxt;
+    }
+
+    public void setRom(File rom) {
+        this.filetxt = rom;
+    }
+
+    
+    public void addTofile() throws IOException {
+        FileWriter fw = null;
+        BufferedWriter bfw = null;
+        
+        try {
+            fw = new FileWriter(filetxt,false);
+            bfw = new BufferedWriter(fw);
+
+            
+            bfw.write(txt);
+            bfw.newLine();
+                
+
+            bfw.flush();
+        } catch (IOException e) {
+        }
+        bfw.close();
+        fw.close();
+        
+    }
+    
+    public void readFile(){
+        Scanner sc = null;
+        txt = "";
+        try {
+            if (filetxt.exists()) {
+                sc = new Scanner(filetxt);
+                
+                while (sc.hasNextLine()) {
+                    String Line = sc.nextLine();
+                    txt = txt + Line +"\n";
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        sc.close();
+    }
+
+    @Override
+    public String toString() {
+        return "adminTextFile{" + "filetxt=" + filetxt + ", txt=" + txt + '}';
+    }
+    
+    
+}
---
+++ src/lab/pkg9p2_hectorrivera/barraProgreso.java
@@ -0,0 +1,46 @@
+package lab.pkg9p2_hectorrivera;
+
+import javax.swing.JProgressBar;
+import javax.swing.JTextArea;
+
+public class barraProgreso extends Thread {
+
+    private JProgressBar progBar;
+    private JTextArea text;
+    private boolean avanzar;
+    private boolean vive;
+    private String string;
+    
+    public barraProgreso() {
+    }
+
+    public barraProgreso(JProgressBar progBar, JTextArea text, String string) {
+        this.progBar = progBar;
+        this.text = text;
+        this.avanzar = true;
+        this.vive = true;
+        this.string = string;
+    }
+
+
+    public void setVida(boolean vive) {
+        this.vive = vive;
+    }
+
+    @Override
+    public void run() {
+        while (vive) {
+                progBar.setValue(progBar.getValue() + 1);
+                //activar y modificar propiedad stringPainted para que esto funciones
+                if (progBar.getValue() == 100) {
+                    vive = false;
+                }
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException ex) {
+            }
+        }
+        text.setText(string);
+    }
+
+}
